dc6b31960aaac65deff7aa0059502967709fbeca
CM-IV <cmathIV@protonmail.com>
2023-12-10 13:59:04 -0600

    First commit

---
+++ justfile
@@ -0,0 +1,6 @@
+run:
+    cargo run
+
+build:
+    RUSTFLAGS="-C target-cpu=native" cargo build --release
+    
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use anyhow::Result;
+use clap::Parser;
+use ollama_rs::{Ollama, generation::completion::request::GenerationRequest};
+use owo_colors::OwoColorize;
+use tokio::io::AsyncWriteExt;
+use tokio_stream::StreamExt;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The question to ask Jeeves
+    #[arg(short, long)]
+    question: String,
+
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let args = Args::parse();
+
+    // For custom values:
+    let ollama = Ollama::new("Your IP".to_string(), 11434);
+
+    let model = "Your model name".to_string();
+
+    let prompt = args.question;
+
+    let str = "\nJeeves is thinking...\n".to_string();
+
+    println!("{}", str.purple());
+
+    let mut stream = ollama.generate_stream(GenerationRequest::new(model, prompt)).await.unwrap();
+
+    let mut stdout = tokio::io::stdout();
+
+    while let Some(res) = stream.next().await {
+        let res = res.unwrap();
+        stdout.write(res.response.as_bytes()).await.unwrap();
+        stdout.flush().await.unwrap();
+    }
+
+    Ok(())
+}

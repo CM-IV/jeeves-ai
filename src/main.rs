9455883105890c7db967fc2c276c6c7170d39909
CM-IV <cmathIV@protonmail.com>
2023-12-13 21:08:32 -0600

    Removed enums

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Result, anyhow};
+use anyhow::Result;
 use clap::{Parser, Subcommand};
 use ollama_rs::{Ollama, generation::completion::request::GenerationRequest};
 use owo_colors::OwoColorize;
@@ -33,32 +33,6 @@ enum Commands {
     
 }
 
-enum AIModel {
-    DeepSeekCoder,
-    OpenHermes25Mistral,
-    StableLmZephyr,
-}
-
-impl AIModel {
-    fn as_str(&self) -> &'static str {
-        match self {
-            AIModel::DeepSeekCoder => "deepseek-coder:6.7b-instruct-q4_0",
-            AIModel::OpenHermes25Mistral => "openhermes2.5-mistral",
-            AIModel::StableLmZephyr => "stablelm-zephyr",
-        }
-    }
-}
-
-// Function to parse the AI model name
-fn parse_ai_model(model: &str) -> Result<AIModel> {
-    match model {
-        "deepseek-coder:6.7b-instruct-q4_0" => Ok(AIModel::DeepSeekCoder),
-        "openhermes2.5-mistral" => Ok(AIModel::OpenHermes25Mistral),
-        "stablelm-zephyr" => Ok(AIModel::StableLmZephyr),
-        _ => Err(anyhow!("Invalid AI model: {}", model)),
-    }
-}
-
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -66,18 +40,16 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     // For custom values:
-    let ollama = Ollama::new("YOUR OLLAMA SERVER".to_string(), 11434);
+    let ollama = Ollama::new("YOUR OLLAMA SERVER ADDRESS".to_string(), 11434);
 
     match args.command {
         Commands::Question { model_name, prompt } => {
-
-            let ai_model = parse_ai_model(&model_name)?;
 
             let str = "\nJeeves is thinking...\n".to_string();
 
             println!("{}", str.purple());
         
-            let mut stream = ollama.generate_stream(GenerationRequest::new(ai_model.as_str().to_owned(), prompt)).await.unwrap();
+            let mut stream = ollama.generate_stream(GenerationRequest::new(model_name, prompt)).await.unwrap();
         
             let mut stdout = tokio::io::stdout();
         
@@ -90,8 +62,10 @@ async fn main() -> Result<()> {
         },
         Commands::List => {
             // Print each AIModel variant
-            for variant in &[AIModel::DeepSeekCoder, AIModel::OpenHermes25Mistral, AIModel::StableLmZephyr] {
-                println!("{}", variant.as_str());
+            let models = ollama.list_local_models().await.unwrap();
+
+            for variant in models.as_slice() {
+                println!("{}", variant.name);
             }
         }
     }
